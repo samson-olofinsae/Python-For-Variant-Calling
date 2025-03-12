5483c6ad8a40a285cf6c7d3675f0fa30e86ac54d
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2025-03-12 16:24:34 +0000

    Update variant-caller.py

--- variant-caller.py
+++ variant-caller.py
@@ -90,5 +90,5 @@ for fq1 in os.path.join (wd, '*_R1.fastq.gz'):
                         f"bcftools index {indexed_variants}",
                         f"bcftools view -v snps {indexed_variants} > {snvs}",
                         f"bcftools view -v indels {indexed_variants} > {indels}"):          
-              
+             
             call(command, shell=True) 
